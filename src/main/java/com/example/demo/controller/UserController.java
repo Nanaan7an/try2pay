c11dd9f955855551e42bddd58b7387f08c1ce4c9
Nanaan7an <2571420180@qq.com>
2024-01-18 11:55:20 +0800

    [接口]post请求

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.pojo.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class UserController {
     /**
      * 查询所有user
      */
+    @PostMapping("/queryAllUser")
     public List<User> queryAllUser() {
         return userService.queryAllUser();
     }
