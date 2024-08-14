e752c9253e73a3d07bb5e6a69a05644cfa631ee7
Nanaan7an <2571420180@qq.com>
2024-08-14 16:53:21 +0800

    [轮子]统一异常处理

---
+++ src/main/java/com/example/demo/exception/BussException.java
@@ -0,0 +1,42 @@
+package com.example.demo.exception;
+
+import com.example.demo.enums.ResultCode;
+import lombok.Getter;
+
+/**
+ * @Author Nanaan
+ * @Date 2024/5/31 11:26
+ * @Description 自定义业务异常
+ */
+
+public class BussException extends RuntimeException {
+    @Getter
+    private String code;
+
+    @Getter
+    private String msg;
+
+    @Getter
+    private String desc;
+
+    /*
+    各种构造函数，方便灵活调用
+     */
+    public BussException(String code, String msg, String desc) {
+        super(msg);
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public BussException(ResultCode resultCode) {
+        super(resultCode.getMsg());
+        this.code = resultCode.getCode();
+    }
+
+    public BussException(ResultCode resultCode, String desc) {
+        super(resultCode.getMsg());
+        this.code = resultCode.getCode();
+        this.desc = desc;
+    }
+
+}
