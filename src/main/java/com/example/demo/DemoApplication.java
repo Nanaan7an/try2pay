0d3af589126164f52d4aa23facd158a38dbf6007
Nanaan7an <2571420180@qq.com>
2025-01-09 14:37:41 +0800

    [Spring]main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        /**
+         * @param: DemoApplication.class 启动类的类型
+         * @param: args String数组，命令行参数，在main方法运行前将参数传入至main；
+         *              args可由使用者传入配置选项（如启动参数--key=value）、业务参数等，在不修改代码的前提下，修改输入参数即可满足不同需求。
+         *              main方法由JVM调用，故参数由JVM指定，默认情况下数组长度为0。
+         *              在idea中，可以在运行配置【Program arguments】中填写需要的参数信息。
+         * @return: ConfigurableApplicationContext Spring容器
+         */
+
         SpringApplication.run(DemoApplication.class, args);
 //        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 //
